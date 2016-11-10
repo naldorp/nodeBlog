c09317f2de7aae1899d270446600c90c8f4831b4
Francinaldo Portela <naldorp@gmail.com>
2016-11-10 13:56:33 -0500

    Added authentication using session and twitter

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+var TWITTER_C_Key = 'IGdsTMbFn4qmMUBKY2XsZXkkC';
+var TWITTER_S_Key = 'yiyHUu3yieOLt3fF9ok6hU7BuiCD4wgrYzP1KulAOeZoYsMxBe';
+
 var express = require("express"),
     http = require("http"),
     path = require("path"),
     routes = require("./routes"),
     mongoskin = require("mongoskin"),
     dbConnectionString = process.env.DATABASE_CON || "mongodb://@localhost:27017/blog",
-    db = mongoskin.db(dbConnectionString, {safe:true}),
-    collections = { articles: db.collection('articles'), users: db.collection('users')};
-  
+    db = mongoskin.db(dbConnectionString, {
+        safe: true
+    }),
+    collections = {
+        articles: db.collection('articles'),
+        users: db.collection('users')
+    };
+
 //express requires  
 var session = require('express-session'),
     logger = require('morgan'),
@@ -14,12 +22,27 @@ var session = require('express-session'),
     cookieParser = require("cookie-parser"),
     bodyParser = require("body-parser"),
     methodOverride = require("method-override");
-    
+
+var everyauth = require("everyauth");
+//everyauth.debug = true;
+everyauth.twitter.consumerKey(TWITTER_C_Key).consumerSecret(TWITTER_S_Key).findOrCreateUser(function(session, accessToken, accessTokenSecret, twitterUserMetada) {
+   if (twitterUserMetada.screen_name === "naldoRP") {
+        session.user = twitterUserMetada;
+        session.admin = true;
+   }
+   return twitterUserMetada;
+}).redirectPath('/admin');
+
+everyauth.everymodule.handleLogout(routes.user.logout);
+everyauth.everymodule.findUserById(function(user, callback) {
+    callback(user);
+})
+
 var app = express();
 
 //middleware(expose all collections to all routes)
-app.use(function(req,res,next){
-    if(!collections.articles || !collections.users) return next(new Error('No collections'));
+app.use(function(req, res, next) {
+    if (!collections.articles || !collections.users) return next(new Error('No collections'));
     req.collections = collections;
     return next();
 });
@@ -27,53 +50,79 @@ app.use(function(req,res,next){
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(methodOverride());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(session({
+    secret: 'And I say, yay',
+    resave: true,
+    saveUninitialized: false
+}));
+app.use(cookieParser('heuheueh'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(everyauth.middleware());
 
-if('development' == app.get('env')){
+//authentication middleware
+app.use(function(req, res, next) {
+    if (req.session && req.session.admin) {
+        res.locals.admin = true;
+    }
+    next();
+});
+
+//authorization middleware
+var authorize = function(req, res, next) {
+    if (req.session && req.session.admin)
+        return next()
+    else
+        return res.sendStatus(401);
+}
+
+if ('development' == app.get('env')) {
     app.use(errorHandler());
 }
 
 //routes
 app.get('/', routes.index);
-app.get('/login',routes.user.login)
-app.post('/login',routes.user.auth)
-app.get('/logout',routes.user.logout)
-app.get('/admin',routes.article.admin)
-app.get('/admin/post',routes.article.post)
-app.post('/admin/post',routes.article.postArticle)
+app.get('/login', routes.user.login)
+app.post('/login', routes.user.auth)
+app.get('/logout', routes.user.logout)
+app.get('/admin', authorize, routes.article.admin)
+app.get('/admin/post', authorize, routes.article.post)
+app.post('/admin/post', authorize, routes.article.postArticle)
 
 //api routes
-app.get('/api/articles',routes.article.list)
-app.post('/api/articles',routes.article.add)
-app.put('/api/articles/:id',routes.article.edit)
-app.delete('/api/articles/:id',routes.article.del)
+app.all('/api', authorize);
+app.get('/api/articles', routes.article.list)
+app.post('/api/articles', routes.article.add)
+app.put('/api/articles/:id', routes.article.edit)
+app.delete('/api/articles/:id', routes.article.del)
 
-app.all('*',function(req,res){
-   res.send(404) ;
+app.all('*', function(req, res) {
+    res.sendStatus(404);
 });
 
 var server = http.createServer(app);
 
 var boot = function() {
     server.listen(app.get('port'), function() {
-        if(process.env.NODE_ENV != 'test')
+        if (process.env.NODE_ENV != 'test')
             console.log('Listening on port ' + app.get('port'));
     });
 }
 
-var shutdown = function(){
+var shutdown = function() {
     server.close();
 }
 
-if(require.main == module){
+if (require.main == module) {
     boot();
 }
-else{
+else {
     console.info("Running index as module");
     exports.boot = boot;
     exports.shutdown = shutdown;
--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,22 @@ exports.login = function(req,res,next){
 };
 
 exports.logout = function(req,res,next){
+  req.session.destroy();
   res.redirect('/');
 };
 
 exports.auth = function(req,res,next){
-  res.redirect('/admin');  
+  if(!req.body.email || !req.body.password)
+    return res.render('login',{error: 'Please enter your email and password'});
+    
+  req.collections.users.findOne({
+    email: req.body.email,
+    password: req.body.password
+  },function(err,user){
+    if(err) return next(err);
+    if(!user) return res.render('login',{error: "invalid email or password"});
+    req.session.user = user;
+    req.session.admin = user.isAdmin;
+    res.redirect('/admin');
+  })
 };
